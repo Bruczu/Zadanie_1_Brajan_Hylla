8d111ae6498ef94edce4e24f7644f3b2a417b0e8
Bruczu <brajan.hylla.bh@gmail.com>
2024-11-15 20:34:08 +0100

    aktualizacja skryptów

--- Zadanie_1_Brajan_Hylla/Assets/Scripts/CameraFollow.cs
+++ Zadanie_1_Brajan_Hylla/Assets/Scripts/CameraFollow.cs
@@ -4,13 +4,8 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform playertransform;
 
-    // Update is called once per frame
     void Update()
     {
         
--- Zadanie_1_Brajan_Hylla/Assets/Scripts/PlayerController.cs
+++ Zadanie_1_Brajan_Hylla/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 5;
+    public Rigidbody rigidbody;
+    
     void Start()
     {
+        rigidbody = GetComponent<Rigidbody>();
         Debug.Log("");
     }
-
-    // Update is called once per frame
     void Update()
     {
-        Debug.Log("");
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            float directionHorizontal = Input.GetAxis("Horizontal");
+            rigidbody.AddForce(directionHorizontal * Time.deltaTime * speed, 0, 0, ForceMode.Impulse);
+        }
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            float directionVertical = Input.GetAxis("Vertical");
+            rigidbody.AddForce(0, 0, directionVertical * Time.deltaTime * speed, ForceMode.Impulse);
+        }
     }
 }
